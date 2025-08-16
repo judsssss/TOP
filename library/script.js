9c3ad6fb34f8331fe0c544059de026d15b735df2
judsssss <jude.apepito.e@gmail.com>
2025-08-16 13:51:59 +0800

    close btn

--- library/script.js
+++ library/script.js
@@ -1,6 +1,7 @@
 const addToLibrary = document.querySelector("#addBtn");
 const inputPop = document.querySelector("#input-pop");
 const confirmAddBtn = document.querySelector("#addBookBtn")
+const closeBtn = document.querySelector("#close");
 
 
 //shows the modal to input
@@ -13,3 +14,12 @@ addToLibrary.addEventListener("click", ()=> {
 confirmAddBtn.addEventListener("click", () => {
     inputPop.classList.remove("visible");
 });
+
+// X close button
+closeBtn.addEventListener("click", () => {
+    inputPop.classList.remove("visible");
+});
+
+function Book(){
+    
+}
